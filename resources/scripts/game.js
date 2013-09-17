45c69d114c18938a0738ebad0bc17ac833813015
thip <david@thecappers.co.uk>
2013-09-17 15:35:27 +0100

    added movement stuff, hid background for now. oh and started structuring actors and scenery classes

--- resources/scripts/game.js
+++ resources/scripts/game.js
@@ -1,7 +1,9 @@
 var bgReady = false;
 var canvas;
-var ctx;
+var context;
 var bgImage;
+
+var holly = new Holly();
 
 var seatImageReady = false;
 var seatImage;
@@ -21,6 +23,9 @@ var edgeWallImage;
 var doorImageReady = false;
 var doorImage;
 
+var hollyImageReady = false;
+var hollyImage;
+
 var mapWidth = 29;
 var mapHeight = 14;
 var mapString =  "w/d/qqqqqqqqqqqqqqqqqqqqq/d/w w///xsssssssstttssssssssx///w w///xsssssssstttssssssssx///w wcccxsssssssstttssssssssxcccw wcccxsssssssstttssssssssxcccw wccc.sssssssstttssssssss.cccw wcccccccccccccccccccccccccccw wccc.-------.ccc.-------.cccw wtttxsssssssstttssssssssxtttw wtttxsssssssstttssssssssxtttw wtttxsssssssstttssssssssxtttw wcccccccccccccccccccccccccccw qqqqqqqqqqqqqqqqqqqqqqqqqqqqq";
@@ -32,7 +37,7 @@ var setUp = function()
 {
 	// Create the canvas
 	canvas = document.createElement("canvas");
-	ctx = canvas.getContext("2d");
+	context = canvas.getContext("2d");
 	canvas.width = mapWidth*32;
 	canvas.height = mapHeight*40;
 
@@ -79,17 +84,163 @@ var setUp = function()
 	};
 	doorImage.src = "resources/images/door.png";
 
+	hollyImage = new Image();
+	hollyImage.onload = function () {
+		hollyImageReady = true;
+	};
+	hollyImage.src = "resources/images/holly.png";
+
 
 
 }
 
+function Position(){
+	var x = 0;
+	var y = 0;
+
+	this.getX = function(){
+		return x;
+	};
+
+	this.getY = function(){
+		return y;
+	};
+
+	this.setPosition = function( newX, newY )
+	{
+		x = newX;
+		y = newY;
+	}
+}
+
+function Drawable()
+{
+	var ready = false;
+	var image = new Image();
+	
+	this.initialise = function(imagePath)
+	{
+		image.src = imagePath;
+		ready = true;
+	}
+	
+
+	this.zIndex = 0;
+
+	this.draw = function(position)
+	{
+		if (ready){
+			context.drawImage(image, position.getX(), position.getY());
+		}	
+	}
+
+}
+
+function Scenery()
+{
+	var ready = false;
+	var position
+	var drawable
+
+	this.setDrawable = function(newDrawable)
+	{
+		drawable = newDrawable;
+	} 
+}
+
+function Actor(newSpeed, newSize){
+	var speed = newSpeed;
+	var size = newSize;
+	var position = new Position();
+	
+	var drawable
+
+	this.setPosition = function(newX,newY)
+	{
+		position.setPosition(newX,newY);
+	}
+
+	this.move = function (newX, newY)
+	{
+		position.setPosition(position.getX() - (newX * speed), position.getY() - (newY * speed));
+	}
+
+	this.setDrawable = function(newDrawable)
+	{
+		drawable = newDrawable;
+	}; 
+
+	this.draw = function()
+	{
+		drawable.draw(position);
+	};
+
+
+	this.getSize = function()
+	{
+		return size;
+	};
+
+	this.isCollidingWith = function(actor)
+	{
+		//get distance between this and the actor
+		//check if it's less than the average size
+		//if it is return true
+		//else false
+	};
+
+
+}
+
+function Holly () {
+
+	var keysDown = {};
+
+	var actor = new Actor(1, 10);
+	actor.setDrawable((function(){
+		var drawable = new Drawable();
+		drawable.initialise("resources/images/holly.png");
+		return drawable;
+	})() )
+
+	actor.setPosition(100,100);
+
+	this.draw = function(){
+		actor.draw();
+	};
+
+	this.update = function()
+	{
+		if (40 in keysDown)
+		{
+			actor.move(0,-1);
+		}
+
+		if (38 in keysDown)
+		{
+			actor.move(0,1);
+		}
+	};
+
+	
+
+	addEventListener("keydown", function (e) {
+		keysDown[e.keyCode] = true;
+	}, false);
+
+	addEventListener("keyup", function (e) {
+		delete keysDown[e.keyCode];
+	}, false);
+	
+}
+
 var render = function () {
 
-	ctx.fillStyle = "rgb(0,0,0)";
-    ctx.fillRect (0,0,canvas.width,canvas.height);
+	context.fillStyle = "rgb(0,0,0)";
+    context.fillRect (0,0,canvas.width,canvas.height);
 
 
-	if (seatImageReady)
+	if (false)//seatImageReady)
 	{
 		var image;
 		for (var ii = 0; ii < map.length; ii++) {
@@ -133,37 +284,45 @@ var render = function () {
 	
 	}
 
+	if (hollyImageReady)
+	{
+
+		holly.draw();
+
+	}
+	
+
 	
 }
 
 var drawStep = function ( x, y)
 {
-	ctx.drawImage(stepImage, y*32, x*40);
+	context.drawImage(stepImage, y*32, x*40);
 }
 
 var drawWall = function ( x, y)
 {
-	ctx.drawImage(wallImage, y*32, x*40);
+	context.drawImage(wallImage, y*32, x*40);
 }
 
 var drawEdgeWall = function ( x, y)
 {
-	ctx.drawImage(edgeWallImage, y*32, x*40);
+	context.drawImage(edgeWallImage, y*32, x*40);
 }
 
 var drawSeat = function ( x, y)
 {
-	ctx.drawImage(seatImage, y*32,(x*40)-15);
+	context.drawImage(seatImage, y*32,(x*40)-15);
 }
 
 var drawCorridoor = function ( x, y)
 {
-	ctx.drawImage(corridoorImage, y*32,x*40);
+	context.drawImage(corridoorImage, y*32,x*40);
 }
 
 var drawDoor = function ( x, y)
 {
-	ctx.drawImage(doorImage, (y-1)*32, x*40);
+	context.drawImage(doorImage, (y-1)*32, x*40);
 }
 
 
@@ -176,6 +335,7 @@ var main = function () {
 	var delta = now - then;
 
 	//update(delta / 1000);
+	holly.update();
 	render();
 
 	then = now;
